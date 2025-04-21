dd266510d311b5e878b63abdc022f46391572ff0
Juan Barragan <juanblow3@gmail.com>
2025-04-21 08:50:36 -0600

    add: Models

---
+++ crud_productos/Models/ProductosContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud_productos.Models;
+
+public partial class ProductosContext : DbContext
+{
+    public ProductosContext()
+    {
+    }
+
+    public ProductosContext(DbContextOptions<ProductosContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producto__3213E83F029E37DC");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(255)
+                .HasColumnName("descripcion");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Precio)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("precio");
+            entity.Property(e => e.Stock).HasColumnName("stock");
+            entity.Property(e => e.Tms)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("tms");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ crud_productos/Models/ViewModels/ProductoVm.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace crud_productos.Models.ViewModels
+{
+    public class ProductoVm
+    {
+        public Producto oProducto { get; set; }
+
+    }
+}
